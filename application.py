1f4eac56e3ffa944bd107b3d62ae7decf833aaaa
AngelinaRS <candy26y_@hotmail.com>
2015-10-12 08:53:15 -0600

    Data input

--- application.py
+++ application.py
@@ -1,3 +1,28 @@
-# Aqui escribe tu codigo
+"Search-Engine"
 
-print "Hello World"
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+
+def reset():
+    """This cleans the screen"""
+    if os.name == "posix": #In linux
+        os.system("clear")
+    elif os.name == ("ce", "nt", "dos"): #In windows
+        os.system("cls")
+reset()
+
+def insert_word():
+    reset()
+    word = raw_input("Insert the word:  ")
+    return word
+
+def first_URL():
+    reset()
+    url1 = raw_input("Insert the first URL:  ")
+    return url1
+
+def second_url():
+    url2 = raw_input("Insert the second URL:  ")
+    return url2
