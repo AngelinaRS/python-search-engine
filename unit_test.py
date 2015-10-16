d545e2870d427e83c7532acdf934dee73e0b3ea1
AngelinaRS <candy26y_@hotmail.com>
2015-10-16 11:07:22 -0600

    with class

--- unit_test.py
+++ unit_test.py
@@ -4,15 +4,22 @@ import unittest
 class SearchEngineTest(unittest.TestCase):
 
 	def test_is_space(self):
-		self.assertEqual(application.is_space(""), True)
-		self.assertEqual(application.is_space(" "), True)
-		self.assertEqual(application.is_space("dulce"), False)
+		searcher = application.SearchEngine()
+		self.assertEqual(searcher.is_space(""), True)
+		self.assertEqual(searcher.is_space(" "), True)
+		self.assertEqual(searcher.is_space("dulce"), False)
 
 	def test_valid_url(self):
-		self.assertEqual(application.valid_url("fdfdfsedeee4fds"), False)
+		searcher = application.SearchEngine()
+		self.assertEqual(searcher.valid_url("fdfdfsedeee4fds"), False)
 
 	def test_count_page(self):
-		self.assertEqual(application.count_page("a", "aba"), 2)
+		searcher = application.SearchEngine()
+		self.assertEqual(searcher.count_page("a", "aba"), 2)
+
+	def test_minuscule(self):
+		searcher = application.SearchEngine()
+		self.assertEqual(searcher.minuscule("Y"), "y")
 
 if __name__ == '__main__':
 	unittest.main()
