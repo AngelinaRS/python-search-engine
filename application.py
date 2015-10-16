7e951b551c90d463043bb08379a33628e1507c79
AngelinaRS <candy26y_@hotmail.com>
2015-10-16 10:44:20 -0600

    with class

--- application.py
+++ application.py
@@ -8,146 +8,159 @@ import sys
 import urllib2
 import re
 
-def reset():
-    """This cleans the screen"""
-    if os.name == "posix": #In linux
-        os.system("clear")
-    elif os.name == ("ce", "nt", "dos"): #In windows
-        os.system("cls")
-reset()
+class SearchEngine(object):
 
-def is_space(word):
-    """This verifies if the word is empty"""
-    if word.isspace() == True or word == "":
-        return True
-    else:
-        return False
+    def __init__(self):
+        pass
 
-def insert_word():
-    """This saves the word given by the user"""
-    reset()
-    space = True
-    while space == True:
+    def reset(self):
+        """This cleans the screen"""
+        if os.name == "posix": #In linux
+            os.system("clear")
+        elif os.name == ("ce", "nt", "dos"): #In windows
+            os.system("cls")
 
-        word = raw_input("Insert the word:  ")
-        space = is_space(word)
-        if space == True:
-            reset()
-            #if the word is empty asks again
-            print "Insert at least a letter to search\n"
+    def is_space(self, word):
+        """This verifies if the word is empty"""
+        if word.isspace() == True or word == "":
+            return True
         else:
-            return word
-    return word
+            return False
 
-def first_url():
-    """This saves the first URL"""
-    url1 = raw_input("\nInsert the first URL:  ")
-    return url1
+    def insert_word(self):
+        """This saves the word given by the user"""
+        self.reset()
+        space = True
+        while space == True:
 
-def second_url():
-    """This saves the second URL"""
-    url2 = raw_input("\nInsert the second URL:  ")
-    return url2
+            word = raw_input("Insert the word:  ")
+            space = self.is_space(word)
+            if space == True:
+                self.reset()
+                #if the word is empty asks again
+                print "Insert at least a letter to search\n"
+            else:
+                return word
+        return word
 
-def valid_url(url):
-    """This verifies if the url is valid"""
+    def first_url(self):
+        """This saves the first URL"""
+        url1 = raw_input("\nInsert the first URL:  ")
+        return url1
 
-    try:
-        page = urllib2.urlopen(url) #This opens the page
-        html = page.read() #This read the page
-        return html
+    def second_url(self):
+        """This saves the second URL"""
+        url2 = raw_input("\nInsert the second URL:  ")
+        return url2
 
-    except:
-        reset()
-        return False #If the url is invalid
+    def valid_url(self, url):
+        """This verifies if the url is valid"""
 
-def count_page(word, url):
-    """This counts the times that appear the word"""
+        try:
+            page = urllib2.urlopen(url) #This opens the page
+            html = page.read() #This read the page
+            return html
 
-    count_url = len(re.findall(word, url))
-    return count_url
+        except:
+            self.reset()
+            return False #If the url is invalid
 
-def more_repetitions(count_url1, count_url2, url1, url2):
-    """This show the page that has the more times the word"""
+    def count_page(self, word, url):
+        """This counts the times that appear the word"""
 
-    if count_url1 > count_url2: #if the first page is highest
-        reset()
-        print "\nThis is the page that countained the highest ocurrence of the word: ", url1
-        print "With %d times" % count_url1
+        count_url = len(re.findall(word, url))
+        return count_url
 
-    elif count_url2 > count_url1: #if the second page is highest
-        reset()
-        print "\nThis is the page that countained the highest ocurrence of the word: ", url2
-        print "With %d times" % count_url2
+    def more_repetitions(self, count_url1, count_url2, url1, url2):
+        """This show the page that has the more times the word"""
 
-    #if both pages has the same quantity
-    elif count_url1 == count_url2 and count_url1 != 0 and count_url2 != 0:
-        reset()
-        print "\nBoth pages has the same quantity of ocurrences"
+        if count_url1 > count_url2: #if the first page is highest
+            self.reset()
+            print "\nThis is the page that countains the highest occurrence of the word: ", url1
+            print "With %d times" % count_url1
 
-    elif count_url1 == 0 and count_url2 == 0:
-        reset()
-        print "\nAny page has the word"
+        elif count_url2 > count_url1: #if the second page is highest
+            self.reset()
+            print "\nThis is the page that countains the highest occurrence of the word: ", url2
+            print "With %d times" % count_url2
 
-def search():
-    """This interacts with the user"""
-    word = insert_word()
-    while True:
-        url1 = first_url() #Insert the first url
-        page1 = valid_url(url1) #Verifies if is valid
-        if page1 == False:
-            print "URL invalid\n"
-        else:
-            while True:
-                url2 = second_url() #Insert the second url
-                page2 = valid_url(url2) #Verifies if is valid
-                if page2 == False:
-                    print "URL invalid\n"
-                else:
-                    count_url1 = count_page(word, page1) #Counts the first page
-                    count_url2 = count_page(word, page2) #Counts the second page
+        #if both pages has the same quantity
+        elif count_url1 == count_url2 and count_url1 != 0 and count_url2 != 0:
+            self.reset()
+            print "\nBoth pages has the same quantity of occurrences"
 
-                    #Show the url that has the highest ocurrence of the word
-                    more_repetitions(count_url1, count_url2, url1, url2)
-                    search_again()
+        elif count_url1 == 0 and count_url2 == 0:
+            self.reset()
+            print "\nAny page has the word"
 
-def search_again():
-    """This ask to the user if want to insert another word"""
-    while True:
-        choose_user = raw_input("\nDo you want to search another word?: y/n ")
-        if choose_user == "y":
-            search() #Searches again
-        elif choose_user == "n":
-            reset()
-            menu() #Return to the menu
-        else:
-            reset()
-            print "Only can write -y- or -n- "
+    def search(self):
+        """This interacts with the user"""
+        word = self.insert_word()
+        while True:
+            url1 = self.first_url() #Insert the first url
+            page1 = self.valid_url(url1) #Verifies if is valid
+            if page1 == False:
+                print "URL invalid\n"
+            else:
+                while True:
+                    url2 = self.second_url() #Insert the second url
+                    page2 = self.valid_url(url2) #Verifies if is valid
+                    if page2 == False:
+                        print "URL invalid\n"
+                    else:
+                        count_url1 = self.count_page(word, page1) #Counts the first page
+                        count_url2 = self.count_page(word, page2) #Counts the second page
 
-def menu():
-    """This saves the menu"""
+                        #Show the url that has the highest ocurrence of the word
+                        self.more_repetitions(count_url1, count_url2, url1, url2)
+                        self.search_again()
 
-    print "1. Search engine"
-    print "2. Exit"
+    def minuscule(self, choose_user):
+        """This converts the election in minuscule"""
+        choose_user = choose_user.lower()
+        return choose_user
 
-    while True:
-        choose_user = raw_input("-")
+    def search_again(self):
+        """This ask to the user if want to insert another word"""
+        while True:
+            choose_user = raw_input("\nDo you want to search another word?: y/n ")
+            choose_user = self.minuscule(choose_user)
 
-        if choose_user == "1":
-            search() #Searches the word
-            raw_input("Press -Enter-  ")
-            reset()
-            menu()
+            if choose_user == "y":
+                self.search() #Searches again
+            elif choose_user == "n":
+                self.reset()
+                self.menu() #Return to the menu
+            else:
+                self.reset()
+                print "Only can write -y- or -n- "
 
-        elif choose_user == "2":
-            reset()
-            sys.exit() #Exit the program
+    def menu(self):
+        """This saves the menu"""
 
-        else:
-            reset()
-            print "//Choose a valid option "
-            menu()
+        print "1. Search engine"
+        print "2. Exit"
+
+        while True:
+            choose_user = raw_input("-")
+
+            if choose_user == "1":
+                self.search() #Searches the word
+                raw_input("Press -Enter-  ")
+                self.reset()
+                self.menu()
+
+            elif choose_user == "2":
+                self.reset()
+                sys.exit() #Exit the program
+
+            else:
+                self.reset()
+                print "//Choose a valid option "
+                self.menu()
+
+MAIN = SearchEngine()
 
 #Test Search Engine
 if __name__ == '__main__':
-    menu()
+    MAIN.menu()
